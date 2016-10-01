2e212a4b93fb1f5dcfc1848c0a9d1a3134426002
NilayKulkarni <typhoscientic@gmail.com>
2016-10-02 02:36:55 +0530

    Nothing

--- app/src/main/java/com/g1ee0k/brainstorm/ViewPagerAdapter.java
+++ app/src/main/java/com/g1ee0k/brainstorm/ViewPagerAdapter.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * Created by geek on 3/9/16.
  */
-public class ViewPagerAdapter extends FragmentPagerAdapter {
+class ViewPagerAdapter extends FragmentPagerAdapter {
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
 
